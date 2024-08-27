3c1b30bc974048ea128428e7bda2ae405467811b
Arman <arman1apollon@gmail.com>
2024-08-27 19:05:32 +0100

    Compeltions and fullscreen gnome terminal

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -13,6 +13,7 @@ return {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
+      local cmp_select = {behavior = cmp.SelectBehavior.Select}
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
@@ -30,7 +31,9 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Enter>"] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+          ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
