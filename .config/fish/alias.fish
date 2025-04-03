b7a05b548c08721ca4e747a63298fce63ee37f73
Arman <arman1apollon@gmail.com>
2025-04-03 15:29:44 +0100

    changed to fish shell, added copilot nvim plugin

---
+++ .config/fish/alias.fish
@@ -0,0 +1,8 @@
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+alias conheadphones='bluetoothctl connect 14:3F:A6:A7:49:63'
+alias discheadphones='bluetoothctl disconnect 14:3F:A6:A7:49:63'
+alias lapmonitorOff='xrandr --output eDP --off'
+alias lapmonitorOn='xrandr --output eDP --auto'
+alias externalMonitor='xrandr --output HDMI-A-0 --auto --right-of eDP --primary'
+
+
---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+source ~/.config/fish/alias.fish
+# export PATH=$PATH:"$PATH:$(go env GOPATH)/bin"
+# export PATH=$PATH:/usr/bin/xc
+
